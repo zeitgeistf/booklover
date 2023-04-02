c0e805d5858ebb539408d0c47b3bbfc4843ac9e5
sf <shawnsfeng@gmail.com>
2023-04-02 16:28:16 -0700

    update package booklover

---
+++ booklover/__init__.py
@@ -0,0 +1,1 @@
+from .booklover import BookLover
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = '0.1.0',
     author = 'zeitgeistf',
     author_email = 'shawnsfeng@gmail.com',
-    packages = ['scr/booklover'],
+    packages = ['booklover'],
     description = 'A package to help booklovers to manage their read books',
     install_requires = [
         "pandas"
