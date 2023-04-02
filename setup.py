3e6c4b88fa6624e1782051e71386d321004022dc
sf <shawnsfeng@gmail.com>
2023-04-02 16:51:46 -0700

    add url to the setup.py file

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = '0.1.0',
     author = 'zeitgeistf',
     author_email = 'shawnsfeng@gmail.com',
+    url = 'https://github.com/zeitgeistf/booklover',
     packages = ['booklover'],
     description = 'A package to help booklovers to manage their read books',
     install_requires = [
